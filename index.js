7b2817164f772b4c19add9f7535a32a45742c4bb
Victor Borov <vitos123@gmail.com>
2018-01-18 02:38:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,32 @@ function add(a,b) {
 
 add(3,5);
 
-function substract(a,b){
+function subtract(a,b){
   return a-b;
 }
-substract (6,3);
+subtract (6,3);
+
+function multiply(a,b){
+  return a*b;
+}
+multiply(4,4);
+
+function divide (a,b){
+  return a/b;
+}
+divide(20,10);
+
+function inc(n){
+  return ++n
+}
+inc(5);
+
+function dec(n){
+  return --n;
+}
+dec(10);
+
+function makeInt(n){
+  return parseInt(n);
+}
+makeInt(10);
